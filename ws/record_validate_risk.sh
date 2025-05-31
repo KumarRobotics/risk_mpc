230e77a5991fe78ea4323fdfb9c1930b12a60d1c
frankgon1627 <fg1333762@gmail.com>
2025-05-31 14:44:11 -0400

    made executables

--- ws/record_validate_risk.sh
+++ ws/record_validate_risk.sh
@@ -1,11 +1,6 @@
 ros2 bag record /tf \
     /tf_static \
     /dlio/odom_node/odom \
-    /cmd_vel \
-    /jackal_velocity_controller/cmd_vel \
-    /jackal_velocity_controller/cmd_vel_unstamped \
-    /joy_teleop/cmd_vel \
-    /joy_teleop/joy \
     /ouster/imu \
     /ouster/imu_packets \
     /ouster/lidar_packets \
@@ -16,13 +11,16 @@ ros2 bag record /tf \
     /obstacle_detection/averaged_positive_obstacle_grid \
     /obstacle_detection/dilated_positive_obstacle_grid \
     /obstacle_detection/risk_map \
-    /obstacle_detection/averaged_risk_map \
+    /obstacle_detection/risk_map_rviz \
+    /obstacle_detection/aggregated_risk_map \
+    /obstacle_detection/aggregated_risk_map_rviz \
     /obstacle_detection/blurred_risk_map \
+    /obstacle_detection/blurred_risk_map_rviz \
     /obstacle_detection/feature_points \
     /obstacle_detection/combined_map \
+    /obstacle_detection/combined_map_rviz \
     /ublox_gps_node/fix \
     /ublox_gps_node/fix_velocity \
     /ublox_gps_node/navpvt \
-    # ADD ZED STUFF
-    
+
     
